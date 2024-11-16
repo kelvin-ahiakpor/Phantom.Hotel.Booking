568a9c3b7735ebf73ca83d7097ee721e6789a1d8
kelvin-ahiakpor <kelvin.ahiakpor@ashesi.edu.gh>
2024-11-16 11:33:25 +0000

    Added files to set up backend

---
+++ db/config.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$dbHost = $_ENV['DB_HOST'];
+$dbName = $_ENV['DB_NAME'];
+$dbUser = $_ENV['DB_USER'];
+$dbPass = $_ENV['DB_PASS'];
+
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
